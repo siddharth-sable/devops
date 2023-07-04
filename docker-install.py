afac686564861704732ed8b01bd250212c69c17c
Siddharth Sabale <siddharth.sable4618@gmail.com>
2023-07-04 13:51:34 +0530

    Create docker-install.py

---
+++ docker-install.py
@@ -0,0 +1,6 @@
+import os
+
+upgrade = "sudo apt-get upgrade"
+os.system(upgrade)
+docker_install_cmds = "curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh ./get-docker.sh"
+os.system(docker_install_cmds)
