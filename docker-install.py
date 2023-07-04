a97770097a1a388368623993ddc4185cb621a62f
Siddharth Sabale <siddharth.sable4618@gmail.com>
2023-07-04 14:04:15 +0530

    Update docker-install.py

--- docker-install.py
+++ docker-install.py
@@ -4,3 +4,4 @@ upgrade = "sudo apt-get upgrade"
 os.system(upgrade)
 docker_install_cmds = "curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh ./get-docker.sh"
 os.system(docker_install_cmds)
+os.system("sudo systemctl start docker.service")
